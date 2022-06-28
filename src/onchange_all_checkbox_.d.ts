2b79ea9a12072adcd49b36100fcaa508deea4c00
Brian Takita <brian.takita@gmail.com>
2022-06-28 10:05:41 -0500

    import { type ... }

--- src/onchange_all_checkbox_.d.ts
+++ src/onchange_all_checkbox_.d.ts
@@ -1,4 +1,4 @@
-import type { WritableAtom_ } from '@ctx-core/nanostores'
+import { type WritableAtom_ } from '@ctx-core/nanostores'
 import type { checkbox_tree_default_Event_T } from './_types'
 export declare function onchange_all_checkbox_<Evt = checkbox_tree_default_Event_T>(
 	store$:WritableAtom_<Record<string, boolean>>, fn:(item_h_selected:Record<string, boolean>, checked:boolean)=>void
